30071fdfe280d62dd9f063da32fb9e3e3ce69f09
Soham Soham <incarnation.3rd@gmail.com>
2025-08-28 23:53:55 +0530

    allow cross site

--- Backend/controllers/admin.controller.js
+++ Backend/controllers/admin.controller.js
@@ -36,6 +36,8 @@ const loginAdmin = async (req, res) => {
     const options = {
       httpOnly: true,
       secure: true,
+      sameSite: "None",
+      maxAge: 24*60*60*1000,
     };
 
     res.status(200)
