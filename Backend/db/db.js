6178871f0126ab124cb67a155d5b8dbacc260988
Soham Soham <incarnation.3rd@gmail.com>
2025-08-28 20:11:04 +0530

    remove bold green

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
-        console.log(`MongoDB Connected || Host: ${connectionInstance.connection.host}`.green.bold);
+        console.log(`MongoDB Connected || Host: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MongoDB Connection Failed :".red.bold,error);
+        console.log("MongoDB Connection Failed :");
     }
 }
 
